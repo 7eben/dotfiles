659630a4c1d03f6ae9dc572ee765a732b8a92966
Zebensui Méndez Corujo <zebensui@gmail.com>
2023-03-12 12:48:02 +0000

    Fix alias

--- home/dot_oh-my-zsh/custom/aliases.zsh
+++ home/dot_oh-my-zsh/custom/aliases.zsh
@@ -1,4 +1,4 @@
-alias kuflow-docker="docker-compose up -d db fusionauth temporal minio fake-smtp-server dockerhost"
+alias kuflow-docker="docker compose up -d db fusionauth temporal minio fake-smtp-server dockerhost"
 alias kuflow-vpn-live-up="wg-quick up eu-01a"
 alias kuflow-vpn-live-down="wg-quick down eu-01a"
 alias kuflow-vpn-sandbox-up="wg-quick up sandbox-01"
